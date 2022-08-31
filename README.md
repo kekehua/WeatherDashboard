d33cb288a3ff01650aa592a7eef5489eecc5ab48
kekehua <100812740+kekehua@users.noreply.github.com>
2022-08-31 10:43:52 -0500

    modifed scripts readme updated

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # WeatherDashboard
- 
+## Description
+This portfolio was built as a basic layout and to learn basic HTML and CSS features. A little JS was used for button functionality.
+
+## Technology
+Key technologies and npm packages used in this project:
+-HTML
+-CSS
+-Bootstrap
+-JS
+
+## Installation
+Viewing the pages is as simple as going to the deployed link below!
+
+## Links
+- [GitHub repository](https://github.com/kekehua/Portfolio)
+- [Deployed application]( https://kekehua.github.io/Portfolio/)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 var APIkey = "9fed56c63d48caf6d924f040bbcd31fc";
 
-var cityInputEl = $("#city-input");
-var searchBtn = $("#search-button");
-var clearBtn = $("#clear-button");
-var pastSearchedCitiesEl = $("#past-searches");
+var cityInputEl = document.getElementById("city-input");
+var searchBtn = document.getElementById("search-button");
+var clearBtn = document.getElementById("clear-button");
+var pastSearchedCitiesEl = document.getElementById("past-searches");
 
 var currentCity;
 
@@ -15,73 +15,67 @@ function getWeather(data) {
             return response.json();
         })
         .then(function(data) {
-            // current weather
-            var currentConditionsEl = $("#currentConditions");
-            currentConditionsEl.addClass("border border-primary");
+            var currentConditionsEl = document.getElementById("currentConditions");
+            currentConditionsEl.classList.add("border");
+            currentConditionsEl.classList.add("border-primary");
 
-            // create city name element and display
-            var cityNameEl = $("<h2>");
-            cityNameEl.text(currentCity);
+            var cityNameEl = document.createElement("h2");
+            const currentCityE1 = document.createTextNode(currentCity);
+            cityNameEl.append(currentCityE1)
             currentConditionsEl.append(cityNameEl);
 
-            // get date from results and display by appending to city name element
             var currentCityDate = data.current.dt;
             currentCityDate = moment.unix(currentCityDate).format("MM/DD/YYYY");
-            var currentDateEl = $("<span>");
-            currentDateEl.text(` (${currentCityDate}) `);
-            cityNameEl.append(currentDateEl);
+            const currentDate = document.createTextNode(currentCityDate);
+            cityNameEl.append(currentDate);
 
-            // get weather icon and display by appending to city name element
-            var currentCityWeatherIcon = data.current.weather[0].icon; // current weather icon
-            var currentWeatherIconEl = $("<img>");
-            currentWeatherIconEl.attr(
-                "src",
-                "http://openweathermap.org/img/wn/" + currentCityWeatherIcon + ".png"
-            );
+
+            var currentCityWeatherIcon = data.current.weather[0].icon; 
+            var currentWeatherIconEl = document.createElement("img");
+            currentWeatherIconEl.src = "http://openweathermap.org/img/wn/" + currentCityWeatherIcon + ".png";
             cityNameEl.append(currentWeatherIconEl);
 
-            // get current temp data and display
             var currentCityTemp = data.current.temp;
-            var currentTempEl = $("<p>");
-            currentTempEl.text(`Temp: ${currentCityTemp}°C`);
+            var currentTempEl = document.createElement("p");
+            currentTempEl = currentCityTemp+ " Celcius  ";
             currentConditionsEl.append(currentTempEl);
 
-            // get current wind speed and display
             var currentCityWind = data.current.wind_speed;
-            var currentWindEl = $("<p>");
-            currentWindEl.text(`Wind: ${currentCityWind} KPH`);
+            var currentWindEl = document.createElement("p");
+            currentWindEl = currentCityWind + " meters/s  ";
             currentConditionsEl.append(currentWindEl);
 
-            // get current humidity and display
             var currentCityHumidity = data.current.humidity;
-            var currentHumidityEl = $("<p>");
-            currentHumidityEl.text(`Humidity: ${currentCityHumidity}%`);
+            var currentHumidityEl = document.createElement("p");
+            currentHumidityEl = currentCityHumidity + "%  "
             currentConditionsEl.append(currentHumidityEl);
 
-            // get current UV index, set background color based on level and display
             var currentCityUV = data.current.uvi;
-            var currentUvEl = $("<p>");
-            var currentUvSpanEl = $("<span>");
+            var currentUvEl = document.createElement("p");
+            var currentUvSpanEl = document.createElement("span");
             currentUvEl.append(currentUvSpanEl);
 
-            currentUvSpanEl.text(`UV: ${currentCityUV}`);
+            currentUvEl= "UV: " + currentCityUV;
 
             if (currentCityUV < 3) {
-                currentUvSpanEl.css({ "background-color": "green", color: "white" });
+                currentUvSpanEl.style.background.color= "green";
+                currentUvSpanEl.style.color="white";
             } else if (currentCityUV < 6) {
-                currentUvSpanEl.css({ "background-color": "yellow", color: "black" });
+                currentUvSpanEl.style.background.color= "yellow";
+                currentUvSpanEl.style.color="black";
             } else if (currentCityUV < 8) {
-                currentUvSpanEl.css({ "background-color": "orange", color: "white" });
+                currentUvSpanEl.style.background.color= "orange";
+                currentUvSpanEl.style.color="white";
             } else if (currentCityUV < 11) {
-                currentUvSpanEl.css({ "background-color": "red", color: "white" });
+                currentUvSpanEl.style.background.color= "red";
+                currentUvSpanEl.style.color="white";
             } else {
-                currentUvSpanEl.css({ "background-color": "violet", color: "white" });
+                currentUvSpanEl.style.background.color= "violet";
+                currentUvSpanEl.style.color="white";
             }
 
             currentConditionsEl.append(currentUvEl);
 
-            // 5 - Day Forecast
-            // create 5 Day Forecast <h2> header
             var fiveDayForecastHeaderEl = $("#fiveDayForecastHeader");
             var fiveDayHeaderEl = $("<h2>");
             fiveDayHeaderEl.text("5-Day Forecast:");
@@ -89,7 +83,6 @@ function getWeather(data) {
 
             var fiveDayForecastEl = $("#fiveDayForecast");
 
-            // get key weather info from API data for five day forecast and display
             for (var i = 1; i <= 5; i++) {
                 var date;
                 var temp;
@@ -105,11 +98,9 @@ function getWeather(data) {
                 wind = data.daily[i].wind_speed;
                 humidity = data.daily[i].humidity;
 
-                // create a card
                 var card = document.createElement("div");
                 card.classList.add("card", "col-2", "m-1", "bg-primary", "text-white");
 
-                // create card body and append
                 var cardBody = document.createElement("div");
                 cardBody.classList.add("card-body");
                 cardBody.innerHTML = `<h6>${date}</h6>
@@ -125,7 +116,6 @@ function getWeather(data) {
     return;
 }
 
-// Display search history as buttons
 function displaySearchHistory() {
     var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
     var pastSearchesEl = document.getElementById("past-searches");
@@ -142,9 +132,9 @@ function displaySearchHistory() {
     return;
 }
 
-// use Open Weather 'Current weather data (API)' to get city coordinates to then send to 'One Call API' to get weather
-function getCoordinates() {
-    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${APIkey}`;
+var getCoordinates = function(currentCity) {
+    console.log(currentCity);
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+currentCity+"&appid="+APIkey;
     var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
 
     fetch(requestUrl)
@@ -175,7 +165,6 @@ function getCoordinates() {
     return;
 }
 
-// handle requst to clear past search history
 function handleClearHistory(event) {
     event.preventDefault();
     var pastSearchesEl = document.getElementById("past-searches");
@@ -201,18 +190,13 @@ function clearCurrentCityWeather() {
     return;
 }
 
-// handle submit of city name by trimming and sending to getCoordinates function, clear HTML display of past weather data, cards, titles
-function handleCityFormSubmit(event) {
-    event.preventDefault();
-    currentCity = cityInputEl.val().trim();
-
-    clearCurrentCityWeather();
-    getCoordinates();
-
-    return;
+const handleCityFormSubmit = function() {
+    var currentCity = document.getElementById("city-input").value;
+    console.log(currentCity);
+        getCoordinates(currentCity);
+    
 }
 
-// When user clicks on city previously searched, an updated forecast will be retrieved and displayed
 function getPastCity(event) {
     var element = event.target;
 
@@ -248,8 +232,10 @@ function getPastCity(event) {
 
 displaySearchHistory();
 
-searchBtn.on("click", handleCityFormSubmit);
+clearBtn.addEventListener("click", clearCurrentCityWeather);
 
-clearBtn.on("click", handleClearHistory);
+searchBtn.addEventListener("click", handleCityFormSubmit);
 
-pastSearchedCitiesEl.on("click", getPastCity);
+clearBtn.addEventListener("click", handleClearHistory);
+
+pastSearchedCitiesEl.addEventListener("click", getPastCity);
